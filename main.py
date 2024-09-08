d58a28d1e2f78ed9c2881d52966c146b512816ed
Mohamed Mahmoud <123865756+MohammedElfar123@users.noreply.github.com>
2024-09-08 03:24:10 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+
+st.set_page_config(
+    page_title="Welcome",
+    page_icon="👋"
+)
+
+st.write("# Welcome to Streamlit! 👋")
+
+st.sidebar.success("Select Demo above.")
